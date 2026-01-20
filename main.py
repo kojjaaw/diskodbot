7c2aa308c0e8b3820afe68ae5fb708ca5ac0de17
kojjaww <mudzanorseptiyas@gmail.com>
2026-01-20 22:06:11 +0800

    kata chat gpt last, pasti berhasil
    kita lihat aja yagesya

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 db = sqlite3.connect("queue.db")
 cursor = db.cursor()
